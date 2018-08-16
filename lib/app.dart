851b762736cb67bc35a8496830ca8dbf96acdb83
Jake Gough <super.jake@hotmail.co.uk>
2018-08-16 12:09:21 +0100

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:SMTApp/viewmodels/contact_viewmodel.dart';
 import 'package:SMTApp/widgets/home_page.dart';
-import 'package:scoped_model/scoped_model.dart';
 
 class SMTApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ScopedModelDescendant<ContactViewModel>(
-      builder: (context, child, model) => MaterialApp(
+    return new MaterialApp(
             title: 'SMTApp',
             theme: new ThemeData(primaryColor: Colors.red),
             home: HomePage(),
-          ),
-    );
+          );
   }
 }
