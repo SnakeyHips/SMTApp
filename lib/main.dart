c86841878322462547f0826ad0b8c84354ac7605
Jake Gough <super.jake@hotmail.co.uk>
2018-08-16 12:09:07 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:SMTApp/app.dart';
 import 'package:SMTApp/viewmodels/contact_viewmodel.dart';
-import 'package:scoped_model/scoped_model.dart';
 
 void main() async {
   await ContactViewModel.loadContacts();
   MaterialPageRoute.debugEnableFadingRoutes = true;
-  runApp(ScopedModel<ContactViewModel>(
-      model: ContactViewModel(), child: SMTApp()));
+  runApp(SMTApp());
 }
