63e1e855ce300af51b9dfd70964b7edc718c85a8
Jake Gough <super.jake@hotmail.co.uk>
2018-08-16 13:58:22 +0100

    Update example_page.dart

--- lib/widgets/departments/example_page.dart
+++ lib/widgets/departments/example_page.dart
@@ -9,18 +9,21 @@ class ExamplePage extends StatefulWidget {
 class ExamplePageState extends State<ExamplePage> {
   @override
   Widget build(BuildContext context) {
-    return Material(
-        color: Colors.white,
-        child: new ListView.builder(
-          padding: new EdgeInsets.only(left: 24.0, right: 24.0, bottom: 24.0),
-          itemCount: 1,
-          itemBuilder: (context, int index) {
-            return new Column(
-              children: <Widget>[
-                PageUtil.pageText("text"),
-              ],
-            );
-          },
-        ));
+    return Hero(
+        tag: "Example Page",
+        child: Material(
+            color: Colors.white,
+            child: new ListView.builder(
+              padding:
+                  new EdgeInsets.only(left: 24.0, right: 24.0, bottom: 24.0),
+              itemCount: 1,
+              itemBuilder: (context, int index) {
+                return new Column(
+                  children: <Widget>[
+                    PageUtil.headerText("text"),
+                  ],
+                );
+              },
+            )));
   }
 }
