879182d69fd10cdbccc465d14ac9a08c4972e558
Jake Gough <super.jake@hotmail.co.uk>
2018-08-16 14:00:22 +0100

    Update example_page.dart

--- lib/widgets/actioncards/example_page.dart
+++ lib/widgets/actioncards/example_page.dart
@@ -37,18 +37,20 @@ class ExamplePageState extends State<ExamplePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-        color: Colors.white,
-        child: new ListView.builder(
-          padding: new EdgeInsets.only(left: 24.0, right: 24.0, bottom: 24.0),
-          itemCount: 1,
-          itemBuilder: (context, int index) {
-            return new Column(
-              children: <Widget>[
-                PageUtil.inputRow(24.0, task1initials, task1time),
-              ],
-            );
-          },
-        ));
+    return Hero(
+        tag: "Example Page",
+        child:  Material(
+          color: Colors.white,
+          child: new ListView.builder(
+            padding: new EdgeInsets.only(left: 24.0, right: 24.0, bottom: 24.0),
+            itemCount: 1,
+            itemBuilder: (context, int index) {
+              return new Column(
+                children: <Widget>[
+                  PageUtil.inputRow(24.0, task1initials, task1time),
+                ],
+              );
+            },
+        )));
   }
 }
